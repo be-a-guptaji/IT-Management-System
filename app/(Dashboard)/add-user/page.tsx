1cc262fe5de047938ea867c58a8c0c68112f3d60
Aryan Baadlas <154962185+be-a-guptaji@users.noreply.github.com>
2025-07-03 01:43:13 +0530

    Add new user form created

--- app/(Dashboard)/add-user/page.tsx
+++ app/(Dashboard)/add-user/page.tsx
@@ -1,11 +1,149 @@
 // @app/(Dashboard)/add-user/page.tsx
 
-const page = () => {
+"use client";
+
+// Components
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// Utility
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  firstName: z.string().min(1),
+  middelName: z.string().min(1),
+  lastName: z.string().min(1),
+  designation: z.string().min(1),
+  macAddress: z.string().min(1),
+  para: z.coerce.number(),
+});
+
+const Page = () => {
+  // Construct a form hook
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      middelName: "",
+      lastName: "",
+      designation: "",
+      macAddress: "",
+      para: 0,
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    // Reset the form after submission
+    console.log(data);
+    form.reset();
+  };
+
   return (
     <>
-      <div className="flex min-h-full flex-wrap items-center justify-center gap-12 p-16"></div>
+      <div className="flex min-h-full flex-wrap items-center justify-center gap-12 p-16">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="middelName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Middle Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Middle name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Last name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="designation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Designation</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Designation" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="macAddress"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>MAC Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="00:1A:2B:3C:4D:5E" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="para"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Para</FormLabel>
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </>
   );
 };
 
-export default page;
+export default Page;
